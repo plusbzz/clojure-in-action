c1341645196c5e18799adc34c9f7175ecb70e481
Raj Bandyopadhyay <plusbzz@gmail.com>
2013-04-17 21:26:14 -0400

    More exercises

--- bin/clojure_in_action/core.clj
+++ bin/clojure_in_action/core.clj
@@ -8,6 +8,7 @@
 (defn my-add [op1 op2] (+ op1 op2))
 (my-add 1 2)
 
+; cond
 (defn cmp [op1 op2]
   (cond
     (> op1 op2) (println "greater")
@@ -15,13 +16,16 @@
     :default (println "equal")))
 (cmp 2 1)
     
+; let
 (let [quoted-list '(add 1 2 3)]
   quoted-list)
 
+; when
 (when (> 1 0)
   (println "hello")
   (println "world"))
 
+; loop and recur
 (defn fact [n]
   (loop [current n 
          val 1]
@@ -30,3 +34,15 @@
       (recur (dec current) (* val current)))))
 (println (fact 10))
 
+; for - list comprehension
+(defn chess-labels []
+  (for [alpha "abcdefgh"
+        num (range 1 20)
+        :when (< num 9)]
+    (str alpha num)))
+(chess-labels)
+
+; Java string ops
+(.split "hello world" " ")
+
+; ISeq interface
